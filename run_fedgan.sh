ca8dc7cfa2fcb38e74f8a07e1c96040e65b598dd
AudeSportisse <45034373+AudeSportisse@users.noreply.github.com>
2023-03-23 12:07:56 +0200

    mon premier commit

--- run_fedgan.sh
+++ run_fedgan.sh
@@ -1,6 +1,5 @@
-#module load conda/5.0.1-python3.6
-#source activate virt_pytorch_conda
-
+module load conda/5.0.1-python3.6
+source activate virt_pytorch_conda
 
 nice -n 5 python3 Fed_gain_main.py
 
